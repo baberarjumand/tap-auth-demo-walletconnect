94d78e528cba1efc1ec2b159c97d119e46e15a2e
baberarjumand <baber.arjumand@gmail.com>
2022-08-23 04:39:21 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tap-auth-demo-walletconnect',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
